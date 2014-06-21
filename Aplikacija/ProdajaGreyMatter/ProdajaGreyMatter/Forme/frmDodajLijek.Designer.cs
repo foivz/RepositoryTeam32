2d0b662c91ad419fdaf0d6e4c8c72cbc5f622f1a
bslibar <barbarabarbi.slibar@gmail.com>
2014-06-21 18:13:10 +0200

    Revert "FrmHelpDodajLijek"
    This reverts commit cfa6f8ec63d92ac5f5dfc130d165fbcad2afe96d.

--- Aplikacija/ProdajaGreyMatter/ProdajaGreyMatter/Forme/frmDodajLijek.Designer.cs
+++ Aplikacija/ProdajaGreyMatter/ProdajaGreyMatter/Forme/frmDodajLijek.Designer.cs
@@ -262,7 +262,6 @@
             this.Name = "frmDodajLijek";
             this.Text = "frmDodajLijek";
             this.Load += new System.EventHandler(this.frmDodajLijek_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pomoc_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPopisLijekova)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.jedinicnamjeraBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tiplijekaBindingSource)).EndInit();
