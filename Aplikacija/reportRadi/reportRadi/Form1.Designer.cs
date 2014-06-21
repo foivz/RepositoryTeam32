fe1eec0f53a6dbda00ff0731ef80b2b683f7f2df
nvarsic <pacific112j.0@hotmail.com>
2014-06-22 01:47:15 +0200

    Kreiran report

---
+++ Aplikacija/reportRadi/reportRadi/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace reportRadi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.DataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.greymatterpiDataSet = new reportRadi.greymatterpiDataSet();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DataTable1TableAdapter = new reportRadi.greymatterpiDataSetTableAdapters.DataTable1TableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.narudzbenicaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.narudzbenicaTableAdapter = new reportRadi.greymatterpiDataSetTableAdapters.narudzbenicaTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greymatterpiDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataTable1BindingSource
+            // 
+            this.DataTable1BindingSource.DataMember = "DataTable1";
+            this.DataTable1BindingSource.DataSource = this.greymatterpiDataSet;
+            // 
+            // greymatterpiDataSet
+            // 
+            this.greymatterpiDataSet.DataSetName = "greymatterpiDataSet";
+            this.greymatterpiDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            reportDataSource4.Name = "DataSet1";
+            reportDataSource4.Value = this.DataTable1BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "reportRadi.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 59);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(991, 373);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DataTable1TableAdapter
+            // 
+            this.DataTable1TableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Narudžbenica:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.narudzbenicaBindingSource;
+            this.comboBox1.DisplayMember = "idNarudzbenice";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(104, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.ValueMember = "idNarudzbenice";
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // narudzbenicaBindingSource
+            // 
+            this.narudzbenicaBindingSource.DataMember = "narudzbenica";
+            this.narudzbenicaBindingSource.DataSource = this.greymatterpiDataSet;
+            // 
+            // narudzbenicaTableAdapter
+            // 
+            this.narudzbenicaTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 432);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greymatterpiDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource DataTable1BindingSource;
+        private greymatterpiDataSet greymatterpiDataSet;
+        private greymatterpiDataSetTableAdapters.DataTable1TableAdapter DataTable1TableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource narudzbenicaBindingSource;
+        private greymatterpiDataSetTableAdapters.narudzbenicaTableAdapter narudzbenicaTableAdapter;
+    }
+}
+
---
+++ Aplikacija/reportRadi/reportRadi/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reportRadi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'greymatterpiDataSet.narudzbenica' table. You can move, or remove it, as needed.
+            this.narudzbenicaTableAdapter.Fill(this.greymatterpiDataSet.narudzbenica);
+            this.DataTable1TableAdapter.FillByID(this.greymatterpiDataSet.DataTable1, 1);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            int id = 0;
+            if (comboBox1.SelectedValue != null)
+                id = int.Parse(comboBox1.SelectedValue.ToString());   
+            if(id != 0)
+                this.DataTable1TableAdapter.FillByID(this.greymatterpiDataSet.DataTable1, id);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
